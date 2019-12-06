41d9e71957dbeccb4fd9147fb019ac0d70d02650
henriquesml <henrique_schmeller@hotmail.com>
2019-12-06 18:13:35 -0300

    Updates

---
+++ precache-manifest.0fa99f21713a061977d763e0f78d4a7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e93994e328dfbaadae2a0cf77cd28a",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "da4cf46172c26b8773b1",
+    "url": "/projects/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ee1576fc958f2be160f4",
+    "url": "/projects/static/js/2.7b0ccfff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/projects/static/js/2.7b0ccfff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da4cf46172c26b8773b1",
+    "url": "/projects/static/js/main.91722e7e.chunk.js"
+  },
+  {
+    "revision": "18940ac017f00a29ef31",
+    "url": "/projects/static/js/runtime-main.c3eb0715.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
